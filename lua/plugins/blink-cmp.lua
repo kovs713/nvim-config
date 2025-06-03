dfc2332a99d097a75b937239014b056e93c49062
kamusari713 <kamusari@yandex.com>
2025-06-03 21:39:55 +0400

    feat: add copilot blink cmp support

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,98 +1,153 @@
 return {
-  'saghen/blink.cmp',
-  event = 'VimEnter',
-  version = '1.*',
-  dependencies = {
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = (function()
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
+  {
+    'saghen/blink.cmp',
+    event = 'VimEnter',
+    version = '1.*',
+    dependencies = {
+      { 'fang2hou/blink-copilot' },
+      {
+        'L3MON4D3/LuaSnip',
+        version = '2.*',
+        build = (function()
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
+        },
+        opts = {},
+      },
+      'folke/lazydev.nvim',
+      'moyiz/blink-emoji.nvim',
+    },
+    --- @module 'blink.cmp'
+    --- @type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = 'enter',
+        ['<Tab>'] = { 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+      },
+
+      appearance = {
+        nerd_font_variant = 'mono',
+        kind_icons = {
+          Copilot = '',
+          Text = '󰉿',
+          Method = '󰊕',
+          Function = '󰊕',
+          Constructor = '󰒓',
+
+          Field = '󰜢',
+          Variable = '󰆦',
+          Property = '󰖷',
+
+          Class = '󱡠',
+          Interface = '󱡠',
+          Struct = '󱡠',
+          Module = '󰅩',
+
+          Unit = '󰪚',
+          Value = '󰦨',
+          Enum = '󰦨',
+          EnumMember = '󰦨',
+
+          Keyword = '󰻾',
+          Constant = '󰏿',
+
+          Snippet = '󱄽',
+          Color = '󰏘',
+          File = '󰈔',
+          Reference = '󰬲',
+          Folder = '󰉋',
+          Event = '󱐋',
+          Operator = '󰪚',
+          TypeParameter = '󰬛',
         },
       },
-      opts = {},
-    },
-    'folke/lazydev.nvim',
-    'moyiz/blink-emoji.nvim',
-  },
-  --- @module 'blink.cmp'
-  --- @type blink.cmp.Config
-  opts = {
-    keymap = {
-      preset = 'enter',
-      ['<Tab>'] = { 'select_next', 'fallback' },
-      ['<S-Tab>'] = { 'select_prev', 'fallback' },
-    },
 
-    appearance = {
-      nerd_font_variant = 'mono',
-    },
-
-    completion = {
-      menu = {
-        draw = {
-          components = {
-            kind_icon = {
-              text = function(ctx)
-                local icon = ctx.kind_icon
-                if ctx.item.source_name == 'LSP' then
-                  local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
-                  if color_item and color_item.abbr ~= '' then
-                    icon = color_item.abbr
+      completion = {
+        menu = {
+          draw = {
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local icon = ctx.kind_icon
+                  if ctx.item.source_name == 'LSP' then
+                    local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                    if color_item and color_item.abbr ~= '' then
+                      icon = color_item.abbr
+                    end
                   end
-                end
-                return icon .. ctx.icon_gap
-              end,
-              highlight = function(ctx)
-                local highlight = 'BlinkCmpKind' .. ctx.kind
-                if ctx.item.source_name == 'LSP' then
-                  local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
-                  if color_item and color_item.abbr_hl_group then
-                    highlight = color_item.abbr_hl_group
+                  return icon .. ctx.icon_gap
+                end,
+                highlight = function(ctx)
+                  local highlight = 'BlinkCmpKind' .. ctx.kind
+                  if ctx.item.source_name == 'LSP' then
+                    local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                    if color_item and color_item.abbr_hl_group then
+                      highlight = color_item.abbr_hl_group
+                    end
                   end
-                end
-                return highlight
-              end,
+                  return highlight
+                end,
+              },
             },
           },
         },
-      },
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
-      ghost_text = {
-        enabled = true,
-      },
-    },
-
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev', 'buffer', 'emoji' },
-      providers = {
-        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-        emoji = {
-          module = 'blink-emoji',
-          name = 'Emoji',
-          score_offset = 15,
-          opts = { insert = true },
-          should_show_items = function()
-            return vim.tbl_contains({ 'gitcommit', 'markdown' }, vim.o.filetype)
-          end,
+        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        ghost_text = {
+          enabled = true,
         },
       },
+
+      sources = {
+        default = { 'copilot', 'lsp', 'path', 'snippets', 'lazydev', 'buffer', 'emoji' },
+        providers = {
+          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          copilot = {
+            name = 'copilot',
+            module = 'blink-copilot',
+            score_offset = 100,
+            async = true,
+          },
+          emoji = {
+            module = 'blink-emoji',
+            name = 'Emoji',
+            score_offset = 15,
+            opts = { insert = true },
+            should_show_items = function()
+              return vim.tbl_contains({ 'gitcommit', 'markdown' }, vim.o.filetype)
+            end,
+          },
+        },
+      },
+
+      snippets = { preset = 'luasnip' },
+
+      fuzzy = { implementation = 'lua' },
+
+      signature = { enabled = true },
     },
-
-    snippets = { preset = 'luasnip' },
-
-    fuzzy = { implementation = 'lua' },
-
-    signature = { enabled = true },
+  },
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
   },
 }
