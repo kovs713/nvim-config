d164ee97598073fec88ecd47366e0481f73cde3d
kamusari713 <kamusari713@yandex.com>
2025-06-30 15:35:40 +0400

    chore: some config refactoring

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -24,16 +24,16 @@ return {
     end, { desc = 'Harpoon m[E]nu' })
 
     --Harpoon marked files
-    vim.keymap.set('n', '<leader>H', function()
+    vim.keymap.set('n', '<C-h>', function()
       harpoon:list():select(1)
     end, { desc = 'Harpoon select 1' })
-    vim.keymap.set('n', '<leader>J', function()
+    vim.keymap.set('n', '<C-j>', function()
       harpoon:list():select(2)
     end, { desc = 'Harpoon select 2' })
-    vim.keymap.set('n', '<leader>K', function()
+    vim.keymap.set('n', '<C-k>', function()
       harpoon:list():select(3)
     end, { desc = 'Harpoon select 3' })
-    vim.keymap.set('n', '<leader>L', function()
+    vim.keymap.set('n', '<C-l>', function()
       harpoon:list():select(4)
     end, { desc = 'Harpoon select 4' })
 
