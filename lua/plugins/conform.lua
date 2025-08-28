ede3ca9fb8007048d06bc376a306b61c9bf77643
kamusari713 <kamusari713@yandex.com>
2025-08-28 18:08:47 +0400

    chore: return auto format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -17,10 +17,10 @@ return {
 
     conform.setup {
       notify_on_error = false,
-      -- format_on_save = {
-      --   timeout_ms = 500,
-      --   lsp_format = "fallback",
-      -- },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
       formatters_by_ft = {
         javascript = { 'prettier' },
         typescript = { 'prettier' },
