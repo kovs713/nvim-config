4ab1e0ad961c4bb8c124cdb6a5157b23421ee4f4
kovs713 <ovsyannikovkostyan@gmail.com>
2025-10-09 09:19:08 +0400

    chore: remove garbage

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,7 @@
 return {
   { 'MagicDuck/grug-far.nvim', enabled = false },
+  { 'folke/noice.nvim', enabled = false },
+  { 'mfussenegger/nvim-lint', enabled = false },
   { 'akinsho/bufferline.nvim', enabled = false },
   { 'nvim-lualine/lualine.nvim', enabled = false },
 }
