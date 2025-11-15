0b31e141dd9a8d859291456643906fc78052348a
kovs713 <ovsyannikovkostyan@gmail.com>
2025-11-15 19:16:28 +0400

    fix: treesitter config

--- lua/plugins/syntax/treesitter.lua
+++ lua/plugins/syntax/treesitter.lua
@@ -4,19 +4,14 @@ return {
   lazy = false,
   build = ':TSUpdate',
   config = function()
-    local treesitter = require 'nvim-treesitter'
+    local treesitter = require 'nvim-treesitter.configs'
     treesitter.setup {
       ensure_installed = { 'lua', 'markdown', 'markdown_inline', 'javascript', 'typescript' },
-
       sync_install = false,
-
       auto_install = true,
-
-      highlight = {
-        enable = true,
-
-        additional_vim_regex_highlighting = false,
-      },
+      highlight = { enable = true, additional_vim_regex_highlighting = false },
+      indent = { enable = true },
+      folds = { enable = true },
     }
   end,
 }
