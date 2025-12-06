77fdbfce01d3b29430701d8e9e5da8b92d1b0588
kovs713 <ovsyannikovkostyan@gmail.com>
2025-12-06 16:42:10 +0400

    chore: remove annoying notifier

--- lua/plugins/navigation/snacks.lua
+++ lua/plugins/navigation/snacks.lua
@@ -4,6 +4,9 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
+      notifier = {
+        enabled = false,
+      },
       picker = {
         matchers = {
           frecency = true,
