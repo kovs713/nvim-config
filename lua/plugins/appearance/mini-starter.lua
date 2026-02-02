b4531e8ffd1e7bdd28bb515ea04f401f2a54caa3
kovs713 <ovsyannikovkostyan@gmail.com>
2026-02-02 04:45:49 +0400

    fix: mini starter issue

--- lua/plugins/appearance/mini-starter.lua
+++ lua/plugins/appearance/mini-starter.lua
@@ -7,19 +7,10 @@ return {
     starter.setup {
       content_hooks = {
         starter.gen_hook.aligning('center', 'center'),
-        starter.gen_hook.adding_bullet('• ', true),
         starter.gen_hook.padding(3, 2),
       },
-      evaluate_single = true,
       items = {
         require('mini.starter').sections.recent_files(4, true),
-        {
-          name = 'Projects',
-          action = function()
-            require('snacks.picker').projects()
-          end,
-          section = ' Choose projects',
-        },
       },
       header = [[
                                __
