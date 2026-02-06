9ab71406be3cba10ac38016c39375a8a42122057
kovs713 <ovsyannikovkostyan@gmail.com>
2026-02-06 21:22:06 +0400

    chore: update harpoon config

--- lua/plugins/navigation/harpoon.lua
+++ lua/plugins/navigation/harpoon.lua
@@ -7,6 +7,7 @@ return {
   },
   config = function()
     local harpoon = require 'harpoon'
+    local map = vim.keymap.set
 
     harpoon:setup {
       global_settings = {
@@ -16,33 +17,36 @@ return {
     }
 
     --Harpoon Nav Interface
-    vim.keymap.set('n', '<leader>a', function()
+    map('n', '<leader>a', function()
       harpoon:list():add()
     end, { desc = 'Harpoon [A]dd file' })
-    vim.keymap.set('n', '<leader>E', function()
+    map('n', '<leader>E', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = 'Harpoon m[E]nu' })
 
     --Harpoon marked files
-    vim.keymap.set('n', '<leader>hh', function()
+    map('n', '<leader>hh', function()
       harpoon:list():select(1)
     end, { desc = 'Harpoon select 1' })
-    vim.keymap.set('n', '<leader>jj', function()
+    map('n', '<leader>jj', function()
       harpoon:list():select(2)
     end, { desc = 'Harpoon select 2' })
-    vim.keymap.set('n', '<leader>kk', function()
+    map('n', '<leader>kk', function()
       harpoon:list():select(3)
     end, { desc = 'Harpoon select 3' })
-    vim.keymap.set('n', '<leader>ll', function()
+    map('n', '<leader>ll', function()
       harpoon:list():select(4)
     end, { desc = 'Harpoon select 4' })
+    map('n', '<leader>;;', function()
+      harpoon:list():select(5)
+    end, { desc = 'Harpoon select 5' })
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set('n', '<M-i>', function()
+    map('n', '<M-i>', function()
       harpoon:list():prev()
     end, { desc = 'Harpoon goto prev' })
 
-    vim.keymap.set('n', '<M-o>', function()
+    map('n', '<M-o>', function()
       harpoon:list():next()
     end, { desc = 'Harpoon goto next' })
   end,
