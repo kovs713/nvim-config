a21522d2a776be4f9e197b361ae6667aa1636a22
kovs713 <ovsyannikovkostyan@gmail.com>
2026-02-10 13:13:44 +0400

    chore: update oil.nvim config

--- lua/plugins/navigation/oil.lua
+++ lua/plugins/navigation/oil.lua
@@ -1,11 +1,20 @@
 return {
   'stevearc/oil.nvim',
   ---@module 'oil'
-  opts = {},
   dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
   lazy = false,
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      default_file_explorer = true,
+      watch_for_changes = true,
+      buf_options = {
+        buflisted = false,
+        bufhidden = 'hide',
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    }
     vim.keymap.set('n', '<leader>ef', '<cmd>Oil<CR>', { desc = 'Open [E]xplorer [O]il' })
   end,
 }
