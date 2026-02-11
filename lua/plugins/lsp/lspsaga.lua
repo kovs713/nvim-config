2fff84459d2c40e007549820b9eebeda4cf601af
kovs713 <ovsyannikovkostyan@gmail.com>
2026-02-12 03:48:23 +0400

    feat: VUE FINALY WORKS

--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -1,6 +1,7 @@
 return {
   'nvimdev/lspsaga.nvim',
   config = function()
+    local map = vim.keymap.set
     require('lspsaga').setup {
       lightbulb = {
         enable = false,
@@ -13,15 +14,19 @@ return {
         border = 'single',
       },
     }
-    vim.keymap.set('n', '<leader>lh', '<cmd>Lspsaga hover_doc<CR>', { desc = '[L]sp [H]over Doc' })
-    vim.keymap.set('n', '<leader>lR', '<cmd>Lspsaga rename<CR>', { desc = '[L]sp [R]ename' })
-    vim.keymap.set('n', '<leader>ld', '<cmd>Lspsaga peek_definition<CR>', { desc = '[L]sp Peek [D]efinition' })
-    vim.keymap.set('n', '<leader>[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>', { desc = '[D]iagnostic next' })
-    vim.keymap.set('n', '<leader>]d', '<cmd>Lspsaga diagnostic_jump_next<CR>', { desc = '[D]iagnostic prev' })
-    vim.keymap.set('n', 'gd', '<cmd>Lspsaga goto_definition<CR>', { desc = '[G]oto [D]efinition' })
-    vim.keymap.set('n', '<leader>lr', function()
+    map('n', '<leader>lh', '<cmd>Lspsaga hover_doc<CR>', { desc = '[L]sp [H]over Doc' })
+    map('n', '<leader>lR', '<cmd>Lspsaga rename<CR>', { desc = '[L]sp [R]ename' })
+    map('n', '<leader>ld', function()
+      require('fzf-lua').lsp_definitions()
+    end, { desc = '[L]sp Peek [D]efinition' })
+    map('n', 'gd', function()
+      require('fzf-lua').lsp_definitions()
+    end, { desc = '[L]sp Peek [D]efinition' })
+    map('n', '<leader>[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>', { desc = '[D]iagnostic next' })
+    map('n', '<leader>]d', '<cmd>Lspsaga diagnostic_jump_next<CR>', { desc = '[D]iagnostic prev' })
+    map('n', '<leader>lr', function()
       require('fzf-lua').lsp_references { silent = true }
     end, { desc = '[L]sp Peek [R]eferences' })
-    vim.keymap.set('n', '<leader>le', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = '[L]sp Diagnostic Float [E]' })
+    map('n', '<leader>le', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = '[L]sp Diagnostic Float [E]' })
   end,
 }
