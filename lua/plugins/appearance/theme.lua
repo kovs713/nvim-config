88dd596d17a5832673cf0619b21535d22e3f2ae7
kovs713 <ovsyannikovkostyan@gmail.com>
2026-02-19 00:27:43 +0400

    feat: add main everforest theme

--- lua/plugins/appearance/theme.lua
+++ lua/plugins/appearance/theme.lua
@@ -1,1 +1,23 @@
-/home/kovs/.config/omarchy/current/theme/neovim.lua
+return {
+  'neanias/everforest-nvim',
+  version = false,
+  lazy = false,
+  priority = 1000,
+  config = function()
+    local everforest = require 'everforest'
+
+    everforest.setup {
+      background = 'hard',
+      transparent_background_level = 0,
+      italics = true,
+      disable_italic_comments = false,
+      inlay_hints_background = 'dimmed',
+      on_highlights = function(hl, palette)
+        hl['@string.special.symbol.ruby'] = { link = '@field' }
+        hl['DiagnosticUnderlineWarn'] = { undercurl = true, sp = palette.yellow }
+      end,
+    }
+
+    everforest.load()
+  end,
+}
