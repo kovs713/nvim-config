9b3bd32a371781fc5c1dc33515052fe4c86284fc
kovs713 <ovsyannikovkostyan@gmail.com>
2026-02-18 16:41:30 +0400

    chore: iduno some changes

--- lua/plugins/appearance/screenkey.lua
+++ lua/plugins/appearance/screenkey.lua
@@ -3,14 +3,8 @@ return {
   lazy = false,
   version = '*',
   config = function()
-    local screenkey = require 'screenkey'
-    local notify = require 'notify'
     local function toggleScreenKey()
       vim.cmd 'Screenkey toggle'
-
-      notify.setup {
-        top_down = screenkey.is_active(),
-      }
     end
 
     vim.keymap.set('n', '<leader>ts', toggleScreenKey, { desc = '[T]oggle [S]creen[K]ey' })
--- lua/plugins/editing/nvim-ts-autotag.lua
+++ lua/plugins/editing/nvim-ts-autotag.lua
@@ -2,31 +2,20 @@ return {
   'windwp/nvim-ts-autotag',
   ft = {
     'html',
-    'xml',
-    'javascript',
-    'typescript',
+    'tsx',
+    'jsx',
     'javascriptreact',
     'typescriptreact',
     'svelte',
     'vue',
   },
   config = function()
-    require('nvim-ts-autotag').setup {
+    local autotag = require 'nvim-ts-autotag'
+    autotag.setup {
       opts = {
         enable_close = true,
         enable_rename = true,
-        enable_close_on_slash = false,
-      },
-      per_filetype = {
-        ['html'] = {
-          enable_close = true,
-        },
-        ['typescriptreact'] = {
-          enable_close = true,
-        },
-        ['vue'] = {
-          enable_close = true,
-        },
+        enable_close_on_slash = true,
       },
     }
   end,
