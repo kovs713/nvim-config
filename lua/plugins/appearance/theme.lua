d312992a253a40b681ad42dd56e7d637eaf5cfea
kovs713 <ovsyannikovkostyan@gmail.com>
2026-03-09 20:46:16 +0400

    chore: change theme

--- lua/plugins/appearance/theme.lua
+++ lua/plugins/appearance/theme.lua
@@ -1,23 +1,37 @@
+-- return {
+--   'neanias/everforest-nvim',
+--   version = false,
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     local everforest = require 'everforest'
+--
+--     everforest.setup {
+--       background = 'hard',
+--       transparent_background_level = 0,
+--       italics = true,
+--       disable_italic_comments = false,
+--       inlay_hints_background = 'dimmed',
+--       on_highlights = function(hl, palette)
+--         hl['@string.special.symbol.ruby'] = { link = '@field' }
+--         hl['DiagnosticUnderlineWarn'] = { undercurl = true, sp = palette.yellow }
+--       end,
+--     }
+--
+--     everforest.load()
+--   end,
+-- }
+
 return {
-  'neanias/everforest-nvim',
-  version = false,
-  lazy = false,
-  priority = 1000,
-  config = function()
-    local everforest = require 'everforest'
+  {
+    'ficcdaf/ashen.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      local ashen = require 'ashen'
 
-    everforest.setup {
-      background = 'hard',
-      transparent_background_level = 0,
-      italics = true,
-      disable_italic_comments = false,
-      inlay_hints_background = 'dimmed',
-      on_highlights = function(hl, palette)
-        hl['@string.special.symbol.ruby'] = { link = '@field' }
-        hl['DiagnosticUnderlineWarn'] = { undercurl = true, sp = palette.yellow }
-      end,
-    }
-
-    everforest.load()
-  end,
+      ashen.load()
+    end,
+  },
 }
