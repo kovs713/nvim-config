09e6038ce15d5c654b921cabeb3440682c1c5789
kovs713 <ovsyannikovkostyan@gmail.com>
2026-03-20 00:42:03 +0400

    update theme

--- lua/plugins/appearance/theme.lua
+++ lua/plugins/appearance/theme.lua
@@ -35,26 +35,36 @@
 --   end,
 -- }
 
+-- return {
+--   'neanias/everforest-nvim',
+--   version = false,
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     local everforest = require 'everforest'
+-- --
+-- --     everforest.setup {
+-- --       background = 'light',
+-- --       transparent_background_level = 0,
+-- --       italics = true,
+-- --       disable_italic_comments = false,
+-- --       inlay_hints_background = 'dimmed',
+-- --       on_highlights = function(hl, palette)
+-- --         hl['@string.special.symbol.ruby'] = { link = '@field' }
+-- --         hl['DiagnosticUnderlineWarn'] = { undercurl = true, sp = palette.yellow }
+-- --       end,
+-- --     }
+-- -- --
+-- -- --     everforest.load()
+-- -- --   end,
+-- -- -- }
+
+-- lua/plugins/rose-pine.lua
+
 return {
-  'neanias/everforest-nvim',
-  version = false,
-  lazy = false,
-  priority = 1000,
+  'rose-pine/neovim',
+  name = 'rose-pine',
   config = function()
-    local everforest = require 'everforest'
-
-    everforest.setup {
-      background = 'light',
-      transparent_background_level = 0,
-      italics = true,
-      disable_italic_comments = false,
-      inlay_hints_background = 'dimmed',
-      on_highlights = function(hl, palette)
-        hl['@string.special.symbol.ruby'] = { link = '@field' }
-        hl['DiagnosticUnderlineWarn'] = { undercurl = true, sp = palette.yellow }
-      end,
-    }
-
-    everforest.load()
+    vim.cmd 'colorscheme rose-pine-dawn'
   end,
 }
