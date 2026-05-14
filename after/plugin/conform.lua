415c57333dcd152d36c3fabe0107c0201fca6cb8
kovs713 <ovsyannikovkostyan@gmail.com>
2026-05-14 10:42:02 +0400

    chore: disable php formatter

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -2,10 +2,15 @@ local conform = require 'conform'
 
 conform.setup {
   notify_on_error = false,
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_format = 'fallback',
-  },
+  format_on_save = function(bufnr)
+    local ignore_filetypes = { 'php' }
+
+    if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+      return
+    end
+
+    return { timeout_ms = 500, lsp_format = 'fallback' }
+  end,
   formatters_by_ft = {
     javascript = { 'prettier', 'oxlint' },
     typescript = { 'prettier', 'oxlint' },
@@ -28,7 +33,6 @@ conform.setup {
     },
     go = { 'gofumpt' },
     rust = { 'rustfmt' },
-    php = { { 'pint', 'php_cs_fixer' } },
     bash = { 'shfmt' },
     sql = { 'pg_format' },
   },
