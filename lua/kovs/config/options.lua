cb01ebe483b5fc378845e26f1f69aa81f81e958f
kovs713 <ovsyannikovkostyan@gmail.com>
2026-09-14 01:00:06 +0400

    add more cyrillic support

--- lua/kovs/config/options.lua
+++ lua/kovs/config/options.lua
@@ -10,6 +10,14 @@ vim.cmd [[
   set imsearch=0
 ]]
 
+vim.opt.langmap = 'ёй,цw,уe,кr,еt,нy,гu,шi,щo,зp,х[,ъ],'
+  .. "фa,ыs,вd,аf,пg,рh,оj,лk,дl,ж\\;,э',"
+  .. 'яz,чx,сc,мv,иb,тn,ьm,ё`,'
+  .. 'ЙQ,ЦW,УE,КR,ЕT,НY,ГU,ШI,ЩO,ЗP,Х{,Ъ},'
+  .. 'ФA,ЫS,ВD,АF,ПG,РH,ОJ,ЛK,ДL,Ж\\:,Э",'
+  .. 'ЯZ,ЧX,СC,МV,ИB,ТN,ЬM,Ё~'
+vim.opt.langremap = true
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
